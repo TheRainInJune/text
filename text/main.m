1f86e8d2b5b9c977b0a36b77df56878f3ce6606f
zhangxinliang <zhangxinliang@zhangxinliangs-iMac.local>
2017-01-22 13:17:24 +0800

    Initial Commit

---
+++ text/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  text
+//
+//  Created by zhangxinliang on 2017/1/22.
+//  Copyright © 2017年 zhangxinliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
